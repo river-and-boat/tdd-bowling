9fcbfc8805d8971af9cd9da5bb0b370c88a4ddb7
RiverAndBoat <842714673@qq.com>
2020-08-03 13:13:48 +0800

    test only strike in first 9 rounds with no continue

--- src/main/java/ScoresTool.java
+++ src/main/java/ScoresTool.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 /**
@@ -8,12 +10,48 @@ import java.util.stream.Stream;
  **/
 public class ScoresTool {
     public Integer calculates(Integer[][] scores) {
-        Integer sum = 0;
+
+        // 总分
+        Integer sumTotal = 0;
+
+        // 存储Spare的索引
+        List<Integer> spareIndex = new ArrayList<>();
+
+        // 存储Strike的索引
+        List<Integer> strikeIndex = new ArrayList<>();
+
+        // 存储正常得分的索引(不超过10)
+        List<Integer> normalIndex = new ArrayList<>();
+
         for (int i = 0; i < scores.length; i++) {
-            for (int j = 0; j < scores[i].length; j++) {
-                sum += scores[i][j];
+            if (scores[i][0] == 10) {
+                // Strike
+                strikeIndex.add(i);
+            } else if (scores[i][0] + scores[i][1] == 10) {
+                // Spare
+                spareIndex.add(i);
+            } else {
+                // Normal
+                normalIndex.add(i);
             }
         }
-        return sum;
+
+        // 正常得分的，直接求和即可
+        for (Integer index : normalIndex) {
+            sumTotal += scores[index][0] + scores[index][1];
+        }
+
+        // 补中得分，除了求当前和，除第十次，还要加下一次的首次得分
+        for (Integer index : spareIndex) {
+            sumTotal += scores[index][0] + scores[index][1] + scores[++index][0];
+        }
+
+        // 全中得分，除了求当前和，除第十次，还要加下一次两次的和
+        for (Integer index : strikeIndex) {
+            Integer nextIndex = index + 1;
+            sumTotal += scores[index][0] + scores[index][1] + scores[nextIndex][0] + scores[nextIndex][1];
+        }
+
+        return sumTotal;
     }
 }
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -4,12 +4,44 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BowlingGameTest {
 
+    private static ScoresTool scoresTool;
+
+    @BeforeEach
+    void init() {
+        scoresTool = new ScoresTool();
+    }
+
     @Test
     void test_all_scores_in_each_round_less_than_10() {
         // Given
         Integer[][] scores = {{1, 2}, {2, 2}, {3, 2}, {3, 3}, {2, 5}, {3, 5}, {6, 3}, {2, 1}, {2, 2}, {3, 2}};
         Integer excepted = 54;
-        ScoresTool scoresTool = new ScoresTool();
+
+        // When
+        Integer totalScores = scoresTool.calculates(scores);
+
+        // Then
+        assertEquals(excepted, totalScores);
+    }
+
+    @Test
+    void test_only_spare_in_first_9_rounds_with_no_continue() {
+        // Given
+        Integer[][] scores = {{1, 2}, {2, 2}, {3, 7}, {3, 3}, {2, 5}, {3, 5}, {6, 4}, {2, 1}, {2, 2}, {3, 2}};
+        Integer excepted = 65;
+
+        // When
+        Integer totalScores = scoresTool.calculates(scores);
+
+        // Then
+        assertEquals(excepted, totalScores);
+    }
+
+    @Test
+    void test_only_strike_in_first_9_rounds_with_no_continue() {
+        // Given
+        Integer[][] scores = {{1, 2}, {10, 0}, {3, 5}, {3, 3}, {10, 0}, {3, 5}, {6, 2}, {2, 1}, {2, 2}, {3, 2}};
+        Integer excepted = 81;
 
         // When
         Integer totalScores = scoresTool.calculates(scores);
